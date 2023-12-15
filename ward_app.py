583eddd886cf4b7d191d9bfae383c7dfa7599394
grethasaur <144260376+grethasaur@users.noreply.github.com>
2023-12-14 18:59:41 -0700

    Update ward_app.py

--- ward_app.py
+++ ward_app.py
@@ -83,13 +83,13 @@ def machine_learning_modeling():
         'Windsor Park YSA Ward',
         'Woodbend Ward'
         ]
-
+    
     # MultiRoute options
     multiroute_options = ['MultiRoute_No', 'MultiRoute_Yes']
 
     # Create dropdowns for user selection
     selected_ward = st.selectbox('Select Ward', ward_options)
-    selected_multiroute = st.selectbox('Select MultiRoute', multiroute_options)
+    selected_multiroute = st.selectbox('Select Did you complete more than 1 route?', multiroute_options)
 
     # Numerical features
     routes_completed = st.slider("Routes Completed", 1, 10, 5) #CompletedRoutes
@@ -97,10 +97,6 @@ def machine_learning_modeling():
     time_spent = st.slider("Time Spent (minutes)", 10, 300, 60)
 
 
-    # Map selected options to one-hot encoded values
-    ward_mapping = {selected_ward: [1 if selected_ward == ward else 0 for ward in ward_options] for selected_ward in ward_options}
-
-    
     # MultiRoute mapping
     multiroute_mapping = {
         'MultiRoute_No': [1, 0],  # Replace with the corresponding one-hot encoded values
@@ -121,8 +117,8 @@ def machine_learning_modeling():
 
         # Prepare input data for prediction, make sure to only put trained features
         # X = [['Stake','MultiRoute','CompletedRoutes','RouteDoors','OverallTime']] from modelling
-        numerical_inputs = [routes_completed,doors_in_route, time_spent]
-
+        numerical_inputs = [routes_completed, doors_in_route, time_spent]
+        
         input_data = numerical_inputs + one_hot_encoded_ward + one_hot_encoded_multiroute
 
         # Make prediction
@@ -178,61 +174,11 @@ def data_collection():
     google_form_url = "https://forms.gle/Sif2hH3zV5fG2Q7P8"#YOUR_GOOGLE_FORM_URL_HERE
     st.markdown(f"[Fill out the form]({google_form_url})")
 
-# Page 6: Chatbot
-import os
-# Set the environment variables from the st.secrets dict
-os.environ['GRADIENT_ACCESS_TOKEN'] = st.secrets["GRADIENT_ACCESS_TOKEN"]
-os.environ['GRADIENT_WORKSPACE_ID'] = st.secrets["GRADIENT_WORKSPACE_ID"]
-
-from gradientai import Gradient
-
-def chatbot():
-    # Streamlit title and description
-    st.title("Chatbot")
-    st.write("Ask a question about the Food Drive!")
-
-    with Gradient() as gradient:
-        base_model = gradient.get_base_model(base_model_slug="nous-hermes2")
-        new_model_adapter = base_model.create_model_adapter(name="interactive_food_drive_model")
-
-
-        user_input = st.text_input("Ask your question:")
-        if user_input and user_input.lower() not in ['quit', 'exit']:
-            samples = [
-            { "inputs": "### Instruction: Provide information about the food drive event. \n\n### Response: The food drive event is a community initiative aimed at collecting food items for those in need. We encourage participants to donate non-perishable food items such as canned goods, rice, pasta, and other essential items. Your contributions will make a difference in the lives of individuals and families facing food insecurity." },
-            { "inputs": "### Instruction: What is the purpose of the food drive campaign? \n\n### Response: The food drive campaign aims to gather food donations to support local communities in need. By donating non-perishable items, you are helping to alleviate hunger and improve the lives of those facing food shortages. Your generous contributions are vital in making this initiative successful." },
-            { "inputs": "### Instruction: Tell me more about how the food drive works. \n\n### Response: The food drive operates by encouraging individuals and organizations to donate non-perishable food items. These items can be dropped off at designated collection points or picked up by volunteers on specified dates. All collected food items are then distributed to local food banks, shelters, and needy families. Your participation in the food drive will contribute to ensuring that everyone in our community has access to nutritious meals." },
-            { "inputs": "### Instruction: How can I get involved in the food drive? \n\n### Response: Getting involved is easy! You can volunteer your time by helping with collections, spreading awareness, or even organizing your own donation event. Every effort counts toward supporting our community members in need." },
-            { "inputs": "### Instruction: What impact will my donation have on the community? \n\n### Response: Your donation directly supports local families and individuals facing food insecurity. By contributing non-perishable items, you're providing essential sustenance and hope to those who need it most." },
-            { "inputs": "### Instruction: Where can I drop off my donations for the food drive? \n\n### Response: You can drop off your donations at designated collection points across the city. Check our website or contact us for specific locations and hours. Your contributions will go a long way in helping our cause!" },
-            { "inputs": "### Instruction: Can my company/organization participate in the food drive? \n\n### Response: Absolutely! We welcome corporate or organizational involvement. You can organize a donation drive within your workplace or collaborate with us to make a substantial impact on our community's well-being." },
-            { "inputs": "### Instruction: What specific food items are most needed for donations? \n\n### Response: While all donations are appreciated, items like canned vegetables, protein-rich foods (canned meat, peanut butter), grains, and hygiene products are highly sought after. Your donations of these items will help meet varied nutritional needs." }
-            ]
-
-            #fine tuning can be removed for faster processing
-            num_epochs = 3
-            count = 0
-            while count < num_epochs:
-              new_model_adapter.fine_tune(samples=samples)
-              count = count + 1
-
-            sample_query = f"### Instruction: {user_input} \n\n### Response:"
-            st.markdown(f"Asking: {sample_query}")
-
-            # after fine-tuning
-            complete_response = new_model_adapter.complete(
-                query=sample_query,
-                max_generated_token_count=100)
-            st.markdown(f"Generated: {complete_response.generated_output}")
-
-        # Delete the model adapter after generating the response
-        new_model_adapter.delete()
-
 
 # Main App Logic
 def main():
     st.sidebar.title("Food Drive App")
-    app_page = st.sidebar.radio("Select a Page", ["Dashboard", "EDA", "ML Modeling", "Neighbourhood Mapping", "Data Collection", "Chatbot"])
+    app_page = st.sidebar.radio("Select a Page", ["Dashboard", "EDA", "ML Modeling", "Neighbourhood Mapping", "Data Collection"])
 
     if app_page == "Dashboard":
         dashboard()
@@ -244,8 +190,6 @@ def main():
         neighbourhood_mapping()
     elif app_page == "Data Collection":
         data_collection()
-    elif app_page == "Chatbot":
-        chatbot()
 
 if __name__ == "__main__":
     main()
